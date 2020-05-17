a42c33a5651c4dc62876dd6601ed80e7e77ccdbb
s-6001 <s-6001@yandex.ru>
2020-05-17 18:11:46 +0500

    Create Popsicles.cs

---
+++ Popsicles/Popsicles.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main()
+        {
+            int siblings, popsicles;
+            siblings = Convert.ToInt32(Console.ReadLine());
+            popsicles = Convert.ToInt32(Console.ReadLine());
+            if (popsicles % siblings == 0) Console.WriteLine("give away");
+            else Console.WriteLine("eat them yourself");
+        }
+    }
+}
